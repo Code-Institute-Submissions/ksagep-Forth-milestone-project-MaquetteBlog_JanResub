ea0620b44e08610669cbf8c01497836c843ccef2
ksagep <sandor.kisgyorgy@gmail.com>
2022-09-06 20:21:35 +0000

    Repair the allowed hosts in the settings.py.

--- maquettenews/settings.py
+++ maquettenews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['maquettenews.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['maquettenews.herokuapp.com', 'localhost']
 
 
 # Application definition
