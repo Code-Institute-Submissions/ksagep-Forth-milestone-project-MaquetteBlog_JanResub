9c7f3065521516a000a696bccfb5267a88323587
ksagep <sandor.kisgyorgy@gmail.com>
2022-09-28 08:33:21 +0000

    Repair spelling mistakes in py files and add text to the README file at Resources and Validation.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status = 1).order_by(' - created_on ')
+    queryset = Post.objects.filter(status=1).order_by('- created_on')
     template_name = 'index.html'
     paginate_by = 6
 
@@ -16,12 +16,11 @@ class PostDetail(View):
 
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status = 1)
-        post = get_object_or_404(queryset, slug = slug)
-        comments = post.comments.filter\
-    (approved = True).order_by(" - created_on")
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("- created_on")
         liked = False
-        if post.likes.filter(id = self.request.user.id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
@@ -39,18 +38,18 @@ class PostDetail(View):
 
     def post(self, request, slug, *args, **kwargs):
 
-        queryset = Post.objects.filter(status = 1)
-        post = get_object_or_404(queryset, slug = slug)
-        comments = post.comments.filter(approved = True).order_by("-created_on")
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
-        if post.likes.filter(id = self.request.user.id).exists():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForm(data = request.POST)
+        comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit = False)
+            comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
         else:
@@ -73,8 +72,8 @@ class PostLike(View):
 
 
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, slug = slug)
-        if post.likes.filter(id = request.user.id).exists():
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
