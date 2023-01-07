4b477b8aced601835e11057987e0dd7e09a9e5ab
ksagep <sandor.kisgyorgy@gmail.com>
2023-01-07 19:46:46 +0000

    Collect and put the trash and pencil icon for updating and deletion.

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@
 from .models import Comment
 from django import forms
 
-
+"""Create a form"""
 class CommentForm(forms.ModelForm):
     class Meta:
         """Determine the type of fields of comment"""
